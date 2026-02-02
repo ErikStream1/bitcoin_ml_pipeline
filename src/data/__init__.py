9bc7b2d77c2162423fa92f424d4adc50d256c442
ErikStream1 <A01639729@exatec.tec.mx>
2026-02-02 10:50:34 -0700

    chore(data): update __init__ imports and __all__ exports

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,5 +1,15 @@
+from .types import (BitsoConfig,
+                    BitsoError,
+                    QuoteSnapshot)
+
+from .providers.bitso_client import BitsoClient
+
 from .load_data import load_btc_data
 from .validate_data import validate_btc_data
 
-__all__ = ["load_btc_data",
+__all__ = ["BitsoConfig",
+           "BitsoError",
+           "QuoteSnapshot",
+            "BitsoClient",
+           "load_btc_data",
            "validate_btc_data"]
