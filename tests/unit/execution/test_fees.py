a90dc144bcd36c335b83073c5c42073de79bf8c3
ErikStream1 <A01639729@exatec.tec.mx>
2026-02-12 14:30:52 -0700

    tests(execution): improve fee tests

--- tests/unit/execution/test_fees.py
+++ tests/unit/execution/test_fees.py
@@ -1,16 +1,16 @@
 from __future__ import annotations
-
 from src.execution import fee_proportional
-import numpy as np
 
-def test_fee_proportional():
-    notional = 100*np.random.random()  
-    fee = np.random.random()  
-    
-    result = fee_proportional(notional = notional, fee_rate = fee)
-    
-    assert isinstance(result, float)
-    assert result > 0
-    
-    
-    
+import pytest
+
+@pytest.mark.parametrize("notional, fee_rate, result",
+                         [(100.0, 0.001, 0.1),
+                          (-250, 0.002,0.5),
+                          0.0, 0.1, 0.0])
+
+def test_fee_proportional(notional: float, 
+                          fee_rate:float, 
+                          result:float
+                          )->None:
+    fee = fee_proportional(notional = notional, fee_rate =fee_rate)
+    assert fee == pytest.approx(result)    
