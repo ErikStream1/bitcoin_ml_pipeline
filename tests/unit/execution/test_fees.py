3073bdee0b0cc573a4460654d6e63509f50975ff
ErikStream1 <A01639729@exatec.tec.mx>
2026-02-12 18:49:04 -0700

    test(execution): fix bug with pytest.mark.parametrize. missing parentheses

--- tests/unit/execution/test_fees.py
+++ tests/unit/execution/test_fees.py
@@ -6,7 +6,7 @@ import pytest
 @pytest.mark.parametrize("notional, fee_rate, result",
                          [(100.0, 0.001, 0.1),
                           (-250, 0.002,0.5),
-                          0.0, 0.1, 0.0])
+                          (0.0, 0.1, 0.0)])
 
 def test_fee_proportional(notional: float, 
                           fee_rate:float, 
