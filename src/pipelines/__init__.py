905e5d7a6ec16803d167df7f5170f4a95689e034
ErikStream1 <A01639729@exatec.tec.mx>
2026-02-18 19:48:52 -0700

    chore(pipeline): update __init__ imports and __all__ exports

--- src/pipelines/__init__.py
+++ src/pipelines/__init__.py
@@ -4,10 +4,12 @@ from .training_pipeline import run_training_pipeline
 from .inference_pipeline import run_inference_pipeline
 from .validation_pipeline import run_model_validation_pipeline
 from .collect_quotes_pipeline import run_collect_quotes_pipeline
+from .realtime_simulation_pipeline import run_realtime_simulation_step
 
 __all__ = ["run_data_pipeline",
            "run_feature_pipeline",
            "run_training_pipeline",
            "run_inference_pipeline",
            "run_model_validation_pipeline",
-           "run_collect_quotes_pipeline"]
+           "run_collect_quotes_pipeline",
+           "run_realtime_simulation_step"]
